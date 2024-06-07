5c6d5ece909b010845ff4eb3085e8de2ab3b2b95
akshataj28 <56750615+akshataj28@users.noreply.github.com>
2024-06-07 16:51:34 +0530

    Add files via upload

---
+++ components/timelineElements.js
@@ -0,0 +1,56 @@
+let timelineElements = [
+
+
+  {
+    id: 1,
+    title: "Arizona State University",
+    location: "Tempe, Arizona - US",
+    description:
+      "Masters in Science Computer Science ",
+    gpa: " GPA: 3.5/4 ",
+    buttonText: "College Projects",
+    date: "August 2023 - May 2025",
+    icon: "school",
+  },
+ 
+  {
+    id: 2,
+    title: "Technology Developer",
+    location: "PwC Mumbai, India",
+    description:
+      " Spearheaded the SAP-Salesforce integration using MuleSoft at PricewaterhouseCoopers India, leading to a 40% reduction in data synchronization time, a 25% increase in sales team productivity, improved data visibility by 50%, and received the 'Client Appreciation' Silver Award for outstanding performance",
+    date: "July 2022 - July 2023",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "Technology Developer Intern",
+    location: "PwC Mumbai, India",
+    description:
+      "Built user interfaces in Salesforce for retrieving order details and facilitating password changes for restricted users, enhancing security and accessibility, which resulted in a 40% increase in user satisfaction and streamlined order management processes.",
+    date: "May 2021 - July 2021",
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Data Science Internship",
+    location: "Cloud Counselage Mumbai, India",
+    description:
+      "Developed a classifier with 95% accuracy to predict student eligibility for internships and produced data visualizations that significantly improved decision-making and project insights.",
+    date: " May 2020 - July 2020",
+    icon: "work",
+  },
+  {
+    id: 5,
+    title: "MkSSS's Cummins College Of Engineering",
+    location: "Pune, MH - INDIA",
+    description:
+      "Bachelors in Technology Computer Science  ",
+    gpa: "GPA: 8.46/10",
+    buttonText: "College Projects",
+    date: "August 2018 - June 2022",
+    icon: "school",
+  },
+];
+
+export default timelineElements;
