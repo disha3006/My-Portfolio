34d848c5d859cf3fe837e351ce01b0b5034e00f6
akshataj28 <56750615+akshataj28@users.noreply.github.com>
2024-06-22 22:30:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-// server used to send send emails
 const app = express();
-app.use(cors());
+app.use(cors()); // Enable CORS for all routes
+
 app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
@@ -28,13 +23,13 @@ contactEmail.verify((error) => {
   }
 });
 
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
+app.post("/contact", (req, res) => {
+  const name = req.body.firstName + ' ' + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
   const mail = {
-    from: name,
+    from: email,
     to: "akshata.jedhe@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
@@ -44,9 +39,12 @@ router.post("/contact", (req, res) => {
   };
   contactEmail.sendMail(mail, (error) => {
     if (error) {
-      res.json(error);
+      console.log(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     } else {
       res.json({ code: 200, status: "Message Sent" });
     }
   });
 });
+
+app.listen(5000, () => console.log("Server Running on port 3000"));
